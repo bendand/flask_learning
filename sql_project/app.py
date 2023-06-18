96c6256af6a26b518f54d888290b6d62c1025e26
Ben <bendanderson13@gmail.com>
2023-06-18 17:07:18 -0500

    restructuring

--- sql_project/app.py
+++ sql_project/app.py
@@ -1,5 +1,5 @@
 import os
-from forms import AddForm, DelForm, AddOwnerForm
+from sql_project.forms import AddForm, DelForm, AddOwnerForm
 from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
