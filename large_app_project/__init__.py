2014380f34044149185fd7084432779d9faafd7b
Ben <bendanderson13@gmail.com>
2023-06-19 16:51:58 -0500

    renamed directory

---
+++ large_app_project/__init__.py
@@ -0,0 +1,26 @@
+from large_app_project.owners.views import owners_blueprint
+from large_app_project.puppies.views import puppies_blueprint
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
+app = Flask(__name__)
+
+# Often people will also separate these into a separate config.py file
+app.config['SECRET_KEY'] = 'mysecretkey'
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+# NOTE! These imports need to come after you've defined db, otherwise you will
+# get errors in your models.py files.
+# Grab the blueprints from the other views.py files for each "app"
+
+app.register_blueprint(owners_blueprint, url_prefix="/owners")
+app.register_blueprint(puppies_blueprint, url_prefix='/puppies')
