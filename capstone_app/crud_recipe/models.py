92b3e945c8d6fb68a88244e4bbecf5f0a7ae0c33
Ben <bendanderson13@gmail.com>
2023-10-30 11:57:37 -0500

    most recent non-working version

--- capstone_app/crud_recipe/models.py
+++ capstone_app/crud_recipe/models.py
@@ -64,8 +64,8 @@ class Recipe(db.Model):
 
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(100))
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    date = db.Column(db.DateTime, default=datetime.utcnow)
 
 
 
